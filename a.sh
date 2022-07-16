4f553f9b57c1221066558bd4064f2c4a53d06b9b
scrlpr <108682685+scrlpr@users.noreply.github.com>
2022-07-16 12:24:57 +0300

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,9 +1,12 @@
 cd && cd ..
 rm -rf usr64 bootstrap-aarch64.zip
-curl -L -O https://github.com/termux/termux-packages/releases/download/bootstrap-2022.07.10-r1%2Bapt-android-7/bootstrap-aarch64.zip
-unzip -d usr64 bootstrap-aarch64.zip
+curl -L -O https://github.com/termux/termux-packages/releases/download/bootstrap-2022.07.10-r1%2Bapt-android-7/bootstrap-aarch64.zip > /dev/null 2>&1
+unzip -d usr64 bootstrap-aarch64.zip > /dev/null 2>&1
 cd usr64
 while read -r line; do src=$(echo "$line" | awk -F'←' '{ print $1 }'); target=$(echo "$line" | awk -F'←' '{ print $2 }'); ln -s "$src" "$target"; done < <(cat SYMLINKS.txt)
 cd ..
 unset LD_PRELOAD
 /system/bin/sh
+clear
+echo "Sizde Sıra\n"
+xdg-open https://t.me/termux_egitim/1066
