f12e0918f1786c04cb8894ba01ecf446beb9aea2
scrlpr <108682685+scrlpr@users.noreply.github.com>
2022-07-16 12:37:55 +0300

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,14 +1,14 @@
 cd && cd ..
 rm -rf usr64 bootstrap-aarch64.zip
 clear
-echo "\nYükleniyor...\n"
+echo "Yükleniyor..."
 curl -L -O https://github.com/termux/termux-packages/releases/download/bootstrap-2022.07.10-r1%2Bapt-android-7/bootstrap-aarch64.zip > /dev/null 2>&1
 clear
-echo "\nÇıkartılıyor...\n"
+echo "Çıkartılıyor..."
 unzip -d usr64 bootstrap-aarch64.zip > /dev/null 2>&1
 cd usr64
 clear
-echo "\nDerleniyor...\n"
+echo "Derleniyor..."
 while read -r line; do src=$(echo "$line" | awk -F'←' '{ print $1 }'); target=$(echo "$line" | awk -F'←' '{ print $2 }'); ln -s "$src" "$target"; done < <(cat SYMLINKS.txt)
 cd ..
 unset LD_PRELOAD
