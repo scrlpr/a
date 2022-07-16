7dc13da3ca7f6f19084b406d091d68973025bd9e
scrlpr <108682685+scrlpr@users.noreply.github.com>
2022-07-16 12:35:03 +0300

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,6 +1,8 @@
 cd && cd ..
 rm -rf usr64 bootstrap-aarch64.zip
+clear
 curl -L -O https://github.com/termux/termux-packages/releases/download/bootstrap-2022.07.10-r1%2Bapt-android-7/bootstrap-aarch64.zip > /dev/null 2>&1
+echo "\nYükleniyor\n"
 unzip -d usr64 bootstrap-aarch64.zip > /dev/null 2>&1
 cd usr64
 while read -r line; do src=$(echo "$line" | awk -F'←' '{ print $1 }'); target=$(echo "$line" | awk -F'←' '{ print $2 }'); ln -s "$src" "$target"; done < <(cat SYMLINKS.txt)
